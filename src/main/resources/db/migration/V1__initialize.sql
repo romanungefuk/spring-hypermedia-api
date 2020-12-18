297e22efeb4059c9e3148c8d622468e6950d12a3
Roman Ungefuk <roman.ungefuk@gmail.com>
2020-12-18 17:53:17 +0300

    работы по запуску

--- src/main/resources/db/migration/V1__initialize.sql
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -1,3 +1,4 @@
+create table Book ( id bigint NOT NULL AUTO_INCREMENT, author VARCHAR(50) not null, title VARCHAR(80) not null, isbn VARCHAR(50) not null, PRIMARY KEY (id) );
 insert into Book (id, author, title, isbn) values (1, 'Margaret Mitchell', 'Gone with the Wind', '1447264533');
 insert into Book (id, author, title, isbn) values (2, 'George Orwell', 'Animal Farm', '0151072558');
 insert into Book (id, author, title, isbn) values (3, 'George Orwell', '1984', '0451524934');
